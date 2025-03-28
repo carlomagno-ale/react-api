dcd70682ebc03846f62cb88ee6a17037dd7ecd9e
carlomagno-ale <carlomagno.ale@gmail.com>
2025-03-28 16:28:10 +0100

    fetch data from API and update component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import { useState } from 'react'
+import { useState } from "react";
 
+const api_endpoint = 'http://localhost:3000/api/v1/posts'
 
-export default function App() {
+function App() {
 
-  console.log(posts)
+  fetch(api_endpoint)
+    .then(res => res.json()
+      .then(data => {
+        console.log(data);
+      }))
+
 
   return (
     <>
-
-      <div>
-        <h1>React API Blog</h1>
-
-      </div>
-
-
+      <h1>react api</h1>
     </>
   )
 }
 
+export default App
