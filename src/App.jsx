d29978a51a53547325120cef765b38f491841acb
carlomagno-ale <carlomagno.ale@gmail.com>
2025-03-28 17:00:47 +0100

    enhance post rendering with Bootstrap layout and card components

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ const api_endpoint = 'http://localhost:3000/api/v1/posts'
 
 export default function App() {
 
-
   const [posts, setPosts] = useState([])
 
   function fetchPosts() {
@@ -18,11 +17,29 @@ export default function App() {
   return (
 
     <>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.slug}>{post.title}</li>
-        ))}
-      </ul>
+      <div className="container py-5">
+
+        <ul>
+          {posts.map((post) => (
+            <li key={post.slug}>{post.title}</li>
+          ))}
+        </ul>
+
+        <div className="row row-cols-3 g-3">
+
+          {posts.map(post => (
+            <div className="col" key={`post-${post.slug}`}>
+              <div className="card">
+                <img className="card-img-top" src={post.image} alt={post.title} />
+                <div className="card-body">
+                  <span className="fs-6 fw-bold">{post.title}</span>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+
+      </div>
     </>
   );
 }
