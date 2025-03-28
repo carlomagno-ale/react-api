3401bcac6bbdacccec38b10c8e83be1de005ad8e
carlomagno-ale <carlomagno.ale@gmail.com>
2025-03-28 15:12:50 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+
+export default function App() {
+
+  console.log(posts)
+
+  return (
+    <>
+
+      <div>
+        <h1>React API Blog</h1>
+
+      </div>
+
+
+    </>
+  )
+}
+
